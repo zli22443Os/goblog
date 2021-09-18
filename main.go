3ec592b8412cbd182a9b9390666d97e45ebc07be
zli22443Os <zli22443@gmial.com>
2021-09-18 14:49:00 +0800

    保存博文到数据库

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 	"unicode/utf8"
@@ -74,6 +75,35 @@ func createTables() {
 	);`
 	_, err := db.Exec(createArticlesSQL)
 	checkError(err)
+}
+
+func saveArticleToDB(title string, body string) (int64, error) {
+
+	var (
+		id   int64
+		err  error
+		rs   sql.Result
+		stmt *sql.Stmt
+	)
+
+	stmt, err = db.Prepare("INSERT INTO articles (title, body) VALUES(?,?)")
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer stmt.Close()
+
+	rs, err = stmt.Exec(title, body)
+	if err != nil {
+		return 0, err
+	}
+
+	if id, err = rs.LastInsertId(); id > 0 {
+		return id, nil
+	}
+
+	return 0, err
 }
 
 func checkError(err error) {
@@ -151,11 +181,15 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if len(errors) == 0 {
-		fmt.Fprint(w, "验证通过！")
-		fmt.Fprintf(w, "title的值为: %v<br/>", title)
-		fmt.Fprintf(w, "title的长度为：%v<br/>", utf8.RuneCountInString(title))
-		fmt.Fprintf(w, "body的值为: %v<br/>", body)
-		fmt.Fprintf(w, "body的长度为：%v<br/>", utf8.RuneCountInString(body))
+		lastInsertId, err := saveArticleToDB(title, body)
+
+		if lastInsertId > 0 {
+			fmt.Fprint(w, "插入成功，ID："+strconv.FormatInt(lastInsertId, 10))
+		} else {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500服务器内部错误")
+		}
 	} else {
 		storeURL, _ := router.Get("articles.store").URL()
 
