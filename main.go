0b280a02c0249be88575bde9b35d22ed4d2719ba
zli22443Os <zli22443@gmial.com>
2021-09-19 15:40:45 +0800

    文章列表

--- main.go
+++ main.go
@@ -29,6 +29,15 @@ type Article struct {
 	ID          int64
 }
 
+func (a Article) Link() string {
+	showURL, err := router.Get("articles.show").URL("id", strconv.FormatInt(a.ID, 10))
+	if err != nil {
+		checkError(err)
+		return ""
+	}
+	return showURL.String()
+}
+
 func main() {
 	initDB()
 	createTables()
@@ -293,7 +302,31 @@ func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "访问文章列表")
+	rows, err := db.Query("SELECT * FROM articles")
+	checkError(err)
+
+	defer rows.Close()
+
+	var articles []Article
+
+	for rows.Next() {
+		var article Article
+
+		err := rows.Scan(&article.ID, &article.Title, &article.Body)
+
+		checkError(err)
+
+		articles = append(articles, article)
+	}
+
+	err = rows.Err()
+	checkError(err)
+
+	tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+
+	checkError(err)
+
+	tmpl.Execute(w, articles)
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
