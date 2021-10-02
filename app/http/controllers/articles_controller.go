e74875f553e2d69f109a7d452a662a1d0e45c302
zli22443Os <zli22443@gmial.com>
2021-10-02 15:05:43 +0800

    文章内容页

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -218,13 +218,29 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "500服务器内部错误")
 		}
 	} else {
-		tmpl, err := template.New("show.gohtml").Funcs(template.FuncMap{
-			"RouteName2URL": route.Name2URL,
-			"Int64ToString": types.Int64ToString,
-		}).ParseFiles("resources/views/articles/show.gohtml")
+
+		// ---  4. 读取成功，显示文章 ---
+
+		// 4.0 设置模板相对路径
+		viewDir := "resources/views"
+
+		// 4.1 所有布局模板文件 Slice
+		files, err := filepath.Glob(viewDir + "/layouts/*.gohtml")
 		logger.LogError(err)
 
-		tmpl.Execute(w, article)
+		// 4.2 在 Slice 里新增我们的目标文件
+		newFiles := append(files, viewDir+"/articles/show.gohtml")
+
+		// 4.3 解析模板文件
+		tmpl, err := template.New("show.gohtml").
+			Funcs(template.FuncMap{
+				"RouteName2URL": route.Name2URL,
+				"Int64ToString": types.Int64ToString,
+			}).ParseFiles(newFiles...)
+		logger.LogError(err)
+
+		// 4.4 渲染模板，将所有文章的数据传输进去
+		tmpl.ExecuteTemplate(w, "app", article)
 	}
 }
 
--- pkg/route/router.go
+++ pkg/route/router.go
@@ -7,18 +7,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var Router *mux.Router
+var route *mux.Router
 
 func SetupRoute(r *mux.Router) {
-	Router = r
+	route = r
 }
 
+// Name2URL 通过路由名称来获取 URL
 func Name2URL(routeName string, pairs ...string) string {
-	url, err := Router.Get(routeName).URL(pairs...)
+	url, err := route.Get(routeName).URL(pairs...)
 	if err != nil {
 		logger.LogError(err)
 		return ""
 	}
+
 	return url.String()
 }
 
