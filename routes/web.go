9594164b46039987565ce11d97e85e5414598352
zli22443Os <zli22443@gmial.com>
2021-10-09 14:52:55 +0800

    退出登录

--- routes/web.go
+++ routes/web.go
@@ -34,6 +34,8 @@ func RegisterWebRoutes(r *mux.Router) {
 
 	r.HandleFunc("/auth/login", auc.Login).Methods("GET").Name("auth.login")
 	r.HandleFunc("/auth/dologin", auc.DoLogin).Methods("POST").Name("auth.dologin")
+	r.HandleFunc("/auth/logout", auc.Logout).Methods("POST").Name("auth.logout")
+
 	// r.Use(middlewares.ForceHTMLMiddleware)
 
 	// 开始会话
